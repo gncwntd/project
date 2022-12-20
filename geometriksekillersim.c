bd040bc2fe28ed3130d7ed9c9b2a7c200a8b72c8
Akif Emre GENÇ <114606260+gncwntd@users.noreply.github.com>
2022-12-20 10:25:02 +0300

    Update geometriksekillersim.c

--- geometriksekillersim.c
+++ geometriksekillersim.c
@@ -12,14 +12,12 @@ void kenarsayisi()
     int ct = 0;
     while(a != -1)
     {
-
         scanf("%d",&a);
         printf("%d . kenar %d\n",i,a);
         i++;
         ct++;
-
     }
-    printf("Bu bir %d'gendir",ct);
+    printf("Sekliniz %d'gendir!\n",ct);
 }
 
 int main()
@@ -29,125 +27,111 @@ int main()
     float q1,q2,q3,q4,w1,w2,w3,w4,r;
 
     printf("\nGEOMETRIK SEKILLERI TANIMA SIMULASYONU\n\n1.UCGEN\n2.DORTGEN\n3.COKGEN\n4.DAIRE\n5.CIKIS\n\n");
+    while(1) {
     printf("LUTFEN ISLEM NUMARASI GIRINIZ:");
     scanf("%d",&sayi);
 
-    if(sayi==1)
-    {
-        printf("LUTFEN KENAR VE ACILARI GIRINIZ:\n");
 
-        printf("Birinci Kenar:\n",x);
-        scanf("%f",&x);
+        if (sayi == 1) {
+            printf("LUTFEN KENAR VE ACILARI GIRINIZ:\n");
 
-        printf("Ikinci kenar:\n",y);
-        scanf("%f",&y);
+            printf("Birinci Kenar:\n", x);
+            scanf("%f", &x);
 
-        printf("Ucuncu kenar:\n",z);
-        scanf("%f",&z);
+            printf("Ikinci kenar:\n", y);
+            scanf("%f", &y);
 
-        printf("1.Aci:\n",k);
-        scanf("%f",&k);
+            printf("Ucuncu kenar:\n", z);
+            scanf("%f", &z);
 
-        printf("2.Aci:\n",l);
-        scanf("%f",&l);
+            printf("1.Aci:\n", k);
+            scanf("%f", &k);
 
-        printf("3.Aci:\n",m);
-        scanf("%f",&m);
+            printf("2.Aci:\n", l);
+            scanf("%f", &l);
 
-        if( fabsf(x - y) < z <= x + y && fabsf(x - z) < y <= x + z && fabsf(y - z) < x <= y + z)
-        {
-            if((x == y) && (y ==z) && (x==z)){
-                printf("Eskenar Ucgen");}
+            printf("3.Aci:\n", m);
+            scanf("%f", &m);
+
+            if (fabsf(x - y) < z <= x + y && fabsf(x - z) < y <= x + z && fabsf(y - z) < x <= y + z) {
+                if ((x == y) && (y == z) && (x == z)) {
+                    printf("Eskenar Ucgen");
+                }
                 //printf("Alanı:%f",);
-            if(((x == y)&& (x!= z) && (y!=z)) || ((x == z) && (x!= y) && (z!=y)) || ((y == z) && (y!= x) && (z!=x))){
-                if((k==90 & l==m) || (m==90 & k==l) || (l==90 & k==m))
-                    printf(" ıkızkenar dık ucgen");}
-                else
+                if (((x == y) && (x != z) && (y != z)) || ((x == z) && (x != y) && (z != y)) ||
+                    ((y == z) && (y != x) && (z != x))) {
+                    if ((k == 90 & l == m) || (m == 90 & k == l) || (l == 90 & k == m))
+                        printf(" ıkızkenar dık ucgen");
+                } else
                     printf("ıkızkenar ucgen");
-            if((x != y) && (x!= z) && (y!=z)){
-                printf("Cesitkenar Ucgen");}
+                if ((x != y) && (x != z) && (y != z)) {
+                    printf("Cesitkenar Ucgen");
+                }
 
-        }
+            } else {
+                printf("Ucgen Belirtmez.");
+            }
+        } else if (sayi == 2) {
+            printf("LUTFEN KENAR VE ACILARI GIRINIZ:\n ");
+
+            printf("Birinci Kenar:\n");
+            scanf("%f", &q1);
+
+            printf("Ikinci Kenar:\n");
+            scanf("%f", &q2);
+
+            printf("Ucuncu Kenar:\n");
+            scanf("%f", &q3);
+
+            printf("Dorduncu Kenar:\n");
+            scanf("%f", &q4);
+
+            printf("1.Aci:\n");
+            scanf("%f", &w1);
+
+            printf("2.Aci:\n");
+            scanf("%f", &w2);
+
+            printf("3.Aci:\n");
+            scanf("%f", &w3);
+
+            printf("4.Aci:\n");
+            scanf("%f", &w4);
+
+            if ((q1 == q2 == q3 == q4) & ((w1 == w3) & (w2 == w4) & ((w1 != w4) & (w2 != w3)))) {
+                printf("Sekliniz Eskenar Dortgen!\n");
+                printf("Alani:%f\n", ((q1 * q2 * sin(w1)) / 2) + ((q3 * q4 * sin(w3)) / 2));
+            } else if (((q1 != q2) & (q3 != q4)) & ((q1 == q3) & (q2 == q4)) &
+                       (w1 == 90 & w2 == 90 & w3 == 90 & w4 == 90)) {
+                printf("Sekliniz Dikdortgen!\n");
+                printf("Alani:%f\n", q1 * q2);
+            } else if ((q1 == q2 == q3 == q4) & (w1 == 90 & w2 == 90 & w3 == 90 & w4 == 90)) {
+                printf("Sekliniz Kare!\n");
+                printf("Alani:%f\n", q1 * q2);
+            } else if ((q1 != q2 != q3 != q4) & ((w1 == w3) & (w2 == w4))) {
+                printf("Sekliniz Paralel Kenar!\n");
+                printf("Alani:%f\n", ((q1 * q2 * sin(w1)) / 2) + ((q3 * q4 * sin(w3)) / 2));
+            } else {
+                printf("Sekliniz Cesitkenar Dortgen!\n");
+                printf("Alani:%f\n", ((q1 * q2 * sin(w1)) / 2) + ((q3 * q4 * sin(w3)) / 2));
+            }
+        } else if (sayi == 3) {
+            kenarsayisi();
+        } else if (sayi == 4) {
+            printf("Lutfen Yaricapi Giriniz:\n");
+            scanf("%f", &r);
+            printf("Dairenin Cevresi:%.2f\n", 2 * PI * r);
+            printf("Dairenin Alani:%.2f\n", PI * r * r);
+
+        } else if (sayi == 5)
+            printf("Yine Bekleriz!\n");
+
         else
-        {
-            printf("Ucgen Belirtmez.");
-        }
-    }
-    else if(sayi==2)
-    {
-        printf("LUTFEN KENAR VE ACILARI GIRINIZ:\n ");
-
-        printf("Birinci Kenar:\n");
-        scanf("%f",&q1);
-
-        printf("Ikinci Kenar:\n");
-        scanf("%f",&q2);
-
-        printf("Ucuncu Kenar:\n");
-        scanf("%f",&q3);
-
-        printf("Dorduncu Kenar:\n");
-        scanf("%f",&q4);
-
-        printf("1.Aci:\n");
-        scanf("%f",&w1);
-
-        printf("2.Aci:\n");
-        scanf("%f",&w2);
-
-        printf("3.Aci:\n");
-        scanf("%f",&w3);
-
-        printf("4.Aci:\n");
-        scanf("%f",&w4);
-
-        if ((q1 == q2 == q3 == q4) & ((w1 == w3) & (w2 == w4) & ((w1!= w4)&(w2!=w3)))) {
-            printf("Sekliniz Eskenar Dortgen!\n");
-            printf("Alani:%f",((q1*q2*sin(w1))/2)+((q3*q4*sin(w3))/2));}
-
-        else if (((q1 != q2) & (q3 != q4)) &((q1==q3)&(q2==q4)) & (w1==90&w2==90&w3==90&w4==90)) {
-            printf("Sekliniz Dikdortgen!\n");
-            printf("Alani:%f",q1*q2);}
-
-        else if ((q1 == q2 == q3 == q4) & (w1==90&w2==90&w3==90&w4==90)) {
-            printf("Sekliniz Kare!\n");
-            printf("Alani:%f",q1*q2);}
-
-        else if ((q1 != q2 != q3 != q4) & ((w1 == w3) & (w2 == w4))) {
-            printf("Sekliniz Paralel Kenar!\n");
-            printf("Alani:%f",((q1*q2*sin(w1))/2)+((q3*q4*sin(w3))/2));}
-
-        else{
-            printf("Sekliniz Cesitkenar Dortgen!\n");
-            printf("Alani:%f",((q1*q2*sin(w1))/2)+((q3*q4*sin(w3))/2));}
-    }
-
-    else if(sayi==3)  //COKGEN
-    {
-        kenarsayisi();
-    }
-
-    else if(sayi==4)
-    {
-        printf("Lutfen Yaricapi Giriniz:");
-        scanf("%f",&r);
-        printf("Dairenin Cevresi:%.2f\n",2*PI*r);
-        printf("Dairenin Alani:%.2f\n",PI*r*r);
+            printf("Gecersiz Islem! Lutfen Tekrar Deneyin.\n");
 
     }
-
-    else if(sayi==5)
-        printf("Yine Bekleriz!");
-
-    else
-        printf("Gecersiz Islem! Lutfen Tekrar Deneyin.");
-
-
 
 
 
 return 0;
 }
-
-
-
